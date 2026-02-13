d7dfda1075a683f58fb3adbf8d5ce9a7713eed82
Shrihari Kulkarni <kshrihari0611@gmail.com>
2026-02-13 19:01:40 +0530

    init

---
+++ src/main/java/com/pl/premierstats/PremierStatsApplication.java
@@ -0,0 +1,13 @@
+package com.pl.premierstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremierStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pl/premierstats/PremierStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.premierstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
